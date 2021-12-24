5c2aa648ad8f65fbf03af7960b54865effc22e59
kate115 <75540720+kate115@users.noreply.github.com>
2021-12-24 02:54:29 -0800

    test

--- api/message/index.js
+++ api/message/index.js
@@ -1,23 +1,27 @@
-// module.exports = async function (context, req) {
-//     context.log('JavaScript HTTP trigger function processed a request.');
-
-//     const name = (req.query.name || (req.body && req.body.name));
-//     const responseMessage = name
-//         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-//         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-//     context.res = {
-//         // status: 200, /* Defaults to 200 */
-//         body: responseMessage
-//     };
-// }
-
 module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-    // const inData = context.bindings.inputDocument;
-    // console.log("inData" , inData);
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    // context.res = {
+    //     // status: 200, /* Defaults to 200 */
+    //     body: responseMessage
+    // };
 
     context.res.json({
-        text: "Hello from the API"
+        text: responseMessage
     });
-};
+}
+
+// module.exports = async function (context, req) {
+
+//     // const inData = context.bindings.inputDocument;
+//     // console.log("inData" , inData);
+
+//     context.res.json({
+//         text: "Hello from the API"
+//     });
+// };
