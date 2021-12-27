a02f97e15ae09e47bdba3927bdb6f8281bf1c5ba
kate115 <75540720+kate115@users.noreply.github.com>
2021-12-27 13:04:00 -0800

    data

--- api/message/index.js
+++ api/message/index.js
@@ -1,9 +1,14 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     console.log(req);
+
+    const melodyData = {name: "Melody", age: 7, gender: "Female"};
+    const irisData = {name: "Iris", age: 3, gender: "Female"};
+
+
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + "! I'm happy to show you this website. "
+        ? `Hello, ${name}! I'm happy to show you this website. `
         : "Hello, friend. I'm happy to show you this website.";
 
     // context.res = {
