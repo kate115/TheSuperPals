774b4b15b627f41a2d57dadab4eb0759ae9e255e
kate115 <75540720+kate115@users.noreply.github.com>
2021-12-27 11:16:20 -0800

    api fun fix

--- api/message/index.js
+++ api/message/index.js
@@ -1,9 +1,9 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
+    console.log(req);
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". I'm happy to show you this website."
+        ? "Hello, " + name + "! I'm happy to show you this website. "
         : "Hello, friend. I'm happy to show you this website.";
 
     // context.res = {
