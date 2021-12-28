39a807a3cc2406d7cf499feba05893e9901c0808
kate115 <75540720+kate115@users.noreply.github.com>
2021-12-27 21:58:46 -0800

    age data lazy automatic

--- api/message/index.js
+++ api/message/index.js
@@ -2,18 +2,22 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     console.log(req);
 
-    const melodyData = {name: "Melody", age: 7, gender: "Female"};
-    const irisData = {name: "Iris", age: 3, gender: "Female"};
+    const melodyData = {name: "Melody", birthYear: 2014, gender: "Female"};
+    const irisData = {name: "Iris", birthYear: 2018, gender: "Female"};
 
     const name = (req.query.name || (req.body && req.body.name));
     let responseMessage = name
         ? `Hello, ${name}! I'm happy to show you this website.`
         : "Hello, friend. I'm happy to show you this website.";
-    console.log(name);
+    let getAge = function(person) {
+        const date = new Date();
+        const yearNow = date.getFullYear();
+        return yearNow - person.birthYear;
+    }
     if(name == melodyData.name) {
-        responseMessage = `${melodyData.name} is a ${melodyData.gender.toLowerCase()}, and she is ${melodyData.age}`;
+        responseMessage = `${melodyData.name} is a ${melodyData.gender.toLowerCase()}, and she is ${getAge(melodyData)}`;
     } else if(name == irisData.name) {
-        responseMessage = `${irisData.name} is a ${irisData.gender}, and she is ${irisData.age}`;
+        responseMessage = `${irisData.name} is a ${irisData.gender}, and she is ${getAge(irisData)}`;
     }
     
     // context.res = {
