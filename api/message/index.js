1be7dea480d9d42bec46ab3dae5523e91e43f6c3
kate115 <75540720+kate115@users.noreply.github.com>
2021-12-27 21:20:38 -0800

    api fun

--- api/message/index.js
+++ api/message/index.js
@@ -5,12 +5,17 @@ module.exports = async function (context, req) {
     const melodyData = {name: "Melody", age: 7, gender: "Female"};
     const irisData = {name: "Iris", age: 3, gender: "Female"};
 
-
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? `Hello, ${name}! I'm happy to show you this website. `
+    let responseMessage = name
+        ? `Hello, ${name}! I'm happy to show you this website.`
         : "Hello, friend. I'm happy to show you this website.";
-
+    console.log(name);
+    if(name == melodyData.name) {
+        responseMessage = `${melodyData.name} is a ${melodyData.gender.toLowerCase()}, and she is ${melodyData.age}`;
+    } else if(name == irisData.name) {
+        responseMessage = `${irisData.name} is a ${irisData.gender}, and she is ${irisData.age}`;
+    }
+    
     // context.res = {
     //     // status: 200, /* Defaults to 200 */
     //     body: responseMessage
