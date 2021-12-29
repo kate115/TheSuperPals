8d8efcd70daf8aa7444e8c8aa2068194aa3f97de
kate115 <75540720+kate115@users.noreply.github.com>
2021-12-29 11:40:16 -0800

    gender reverse by making function and for loop for data looking

--- api/message/index.js
+++ api/message/index.js
@@ -2,40 +2,36 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     console.log(req);
 
-    const melodyData = {name: "Melody", birthYear: 2014, gender: "Female"};
-    const irisData = {name: "Iris", birthYear: 2018, gender: "Female"};
-
     const name = (req.query.name || (req.body && req.body.name));
-    let responseMessage = name
-        ? `Hello, ${name}! I'm happy to show you this website.`
-        : "Hello, friend. I'm happy to show you this website.";
-    let getAge = function(person) {
+    
+    const getAge = function(person) {
         const date = new Date();
         const yearNow = date.getFullYear();
         return yearNow - person.birthYear;
     }
-    if(name == melodyData.name) {
-        responseMessage = `${melodyData.name} is a ${melodyData.gender.toLowerCase()}, and she is ${getAge(melodyData)}`;
-    } else if(name == irisData.name) {
-        responseMessage = `${irisData.name} is a ${irisData.gender}, and she is ${getAge(irisData)}`;
+
+    const reverseGender = function(gender) {
+        if(gender == "Female") {
+            return "Male";
+        } else if(gender == "Male") {
+            return "Female";
+        }
     }
-    
-    // context.res = {
-    //     // status: 200, /* Defaults to 200 */
-    //     body: responseMessage
-    // };
+
+    const inData = context.bindings.inputDocument;
+    console.log(inData);
+
+    let responseMessage = "";
+    for (let i = 0; i < inData.length; i++) {
+        if(name == inData[i].name) {
+            responseMessage = `${inData[i].name} is not a ${reverseGender(inData[i].gender).toLowerCase()}, and she is ${getAge(inData[i])}.`;
+        }
+    }
+
 
     context.res.json({
-        text: responseMessage
+        text: responseMessage,
+        dbData: inData
     });
 }
 
-// module.exports = async function (context, req) {
-
-//     // const inData = context.bindings.inputDocument;
-//     // console.log("inData" , inData);
-
-//     context.res.json({
-//         text: "Hello from the API"
-//     });
-// };
