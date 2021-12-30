c2637f83c7e1661116b2429f24613a281446b281
kate115 <75540720+kate115@users.noreply.github.com>
2021-12-30 15:54:19 -0800

    api for members

--- api/getMembers/index.js
+++ api/getMembers/index.js
@@ -1,13 +1,23 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    console.log(req);
 
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    const inData = context.bindings.inputDocument;
+    console.log(inData);
+
+    let responseMessage = "";
+    for (let i = 0; i < inData.length; i++) {
+        if(i == inData.length - 1) {
+            responseMessage += inData[i].name + ".";
+        } else {
+            responseMessage += inData[i].name + ", ";
+        }
+    }
+
+    context.res.json({
+        text: responseMessage,
+        dbData: inData
+    });
 }
