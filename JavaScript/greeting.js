5ca2d10a9df7402d6c9f9f7e00f3d2a84100f695
kate115 <75540720+kate115@users.noreply.github.com>
2022-01-02 16:38:51 -0800

    bug fix try #3 fail

--- JavaScript/greeting.js
+++ JavaScript/greeting.js
@@ -16,3 +16,23 @@ if(hourNow > 18) {
 
 // Write the greeting
 document.write(greeting);
+
+// TEST
+
+var buttonMel = document.getElementById("melodyButton");
+var buttonRyan = document.getElementById("ryanButton");
+var buttonIsabela = document.getElementById("isabelaButton");
+var textDataBox = document.getElementById("infoBox");
+var melody = function() {
+    textDataBox.innerHTML = "<h1>Melody is a female and she is 8.</h1>";
+}
+var ryan = function() {
+    textDataBox.innerHTML = "<h1>Ryan is a male and he is 8.</h1>";
+}
+var isabela = function() {
+    textDataBox.innerHTML = "<h1>Isabela is a female and she is 7.</h1>";
+}
+
+buttonMel.addEventListener("click", melody);
+buttonRyan.addEventListener("click", ryan);
+buttonIsabela.addEventListener("click", isabela);
