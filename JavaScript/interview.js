7e28e03ed933b670beb241a34b4de6bb9513735c
kate115 <75540720+kate115@users.noreply.github.com>
2022-01-15 13:02:01 -0800

    javascript clean part 2

--- JavaScript/interview.js
+++ JavaScript/interview.js
@@ -1,21 +1,11 @@
-// This will be used for the news interviews. It'll have a class, and it'll show the answer to it.
-var question1 = document.getElementById("question1");
-var question2 = document.getElementById("question2");
-var question3 = document.getElementById("question3");
+const questions = document.getElementsByClassName("question");
+const answers = document.getElementsByClassName("answer");
 
-var answer1 = document.getElementById("answer1");
-var answer2 = document.getElementById("answer2");
-var answer3 = document.getElementById("answer3");
-
-answer1.style.display = "none";
-answer2.style.display = "none";
-answer3.style.display = "none";
-
-const questions = [question1, question2, question3];
-const answers = [answer1, answer2, answer3];
+console.log(`${questions}, ${answers}`)
 
 for (let i = 0; i < questions.length; i++) {
     questionEvent(questions[i], answers[i]);
+    answers[i].style.display = "none";
 }
 
 function questionEvent(question, answer) {
