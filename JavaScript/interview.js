d14145aa165592bd8c0e4bf4411dbf695b1d63b2
kate115 <75540720+kate115@users.noreply.github.com>
2022-01-16 12:16:31 -0800

    interview un-hardcode

--- JavaScript/interview.js
+++ JavaScript/interview.js
@@ -1,12 +1,18 @@
 const QAndA = [
-    {question: "Is this a question?", answer: "Yes"},
-    {question: "Is it not?", answer: "No"}
+    {question: "Why have you made this website?", answer: "We want to alert that a new company is coming out!"},
+    {question: "What's Slug and Leaf?", answer: "It's a story we're working on that's about a Slug and Leaf!"},
+    {question: "Can you write a story?", answer: "Yeah, because I'm working on Slug and Leaf!"}
 ]
 
-// var test = document.getElementById("testP");
-// var element = document.createElement("p");
-// element.innerText = "Hi, this is for TEST ONLY SO DO NOT LOOK"
-// test.appendChild(element);
+var interviewDiv = document.getElementById("interview");
+interviewDiv.innerHTML = '';
+
+for (let i = 0; i < QAndA.length; i++) {
+    interviewDiv.innerHTML += `
+        <li class="question"><b>Question:</b> ${QAndA[i].question}</li>
+        <li class="answer"><b>Melody:</b> ${QAndA[i].answer}</li>
+    `;
+}
 
 const questions = document.getElementsByClassName("question");
 const answers = document.getElementsByClassName("answer");
